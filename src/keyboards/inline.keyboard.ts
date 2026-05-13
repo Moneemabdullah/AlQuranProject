187b45e589baf870bdf38d1fbc897fde5d02c273
Moneemabdullh <moneem.all.abdullah@gmail.com>
2026-05-13 23:32:27 +0600

    fix: remove empty button from inline keyboard

--- src/keyboards/inline.keyboard.ts
+++ src/keyboards/inline.keyboard.ts
@@ -7,18 +7,7 @@ export function getAyahInlineKeyboard(
   return {
     inline_keyboard: [
       [
-        // {
-        //   text: '📚 Tafsir',
-        //   callback_data: `tafsir_${chapter}:${verse}`,
-        // },
-        // {
-        //   text: '🔊 Audio',
-        //   callback_data: `audio_${chapter}:${verse}`,
-        // },
-        // {
-        //   text: '❤️ Favorite',
-        //   callback_data: `favorite_${chapter}:${verse}`,
-        // },
+        
       ],
     ],
   };
@@ -29,12 +18,7 @@ export function getMoreResultsKeyboard(
 ): TelegramBot.InlineKeyboardMarkup {
   return {
     inline_keyboard: [
-      // [
-      //   {
-      //     text: '➡️ More Results',
-      //     callback_data: `more_results_${offset}`,
-      //   },
-      // ],
+      
     ],
   };
 }
